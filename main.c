29ed2e40cc706fde6d6f84df8f04d6aad016a38e
Bakhmutov_Ivan <vanya.bahmutov@yandex.ru>
2026-03-16 16:02:28 +0700

    Created string compare

--- main.c
+++ main.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
 
+int my_strcmp(char* a, char* b) {
+        while (*a && *b && *a == *b) {
+                a++;
+                b++;
+        }
+        return (char)a - (char)b;
+}
 int main()
 {
-    printf("Hello World!\n");
+
     return 0;
 }
+
