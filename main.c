faa3c1908020c46a0bd58f5af2ca6f2da783b30c
Bakhmutov_Ivan <vanya.bahmutov@yandex.ru>
2026-03-16 15:59:02 +0700

    Created a function that solves task 2. Added test case

--- main.c
+++ main.c
@@ -1,7 +1,50 @@
-#include <stdio.h>
+#include<stdio.h>
+#include<string.h>
+#define int_max 2147483647
+int convert_str_to_num(char* num) {
+        long long convertedNum = 0;
+        int nLen = strlen(num);
+        int sign = 0;
+        int i = 0;
 
-int main()
-{
-    printf("Hello World!\n");
-    return 0;
+        if (nLen > 11) {
+                printf("Error: Number is too big or too small\n");
+                return -1;
+        }
+        if (num[0] == '-') {
+                sign = 1;
+                i = 1;
+        }
+        if (num[0] == '+') {
+                i = 1;
+        }
+
+        for (; i < nLen; i++) {
+                if (num[i] < '0' || num[i] > '9') {
+                        printf("Error: Parameter contains not digit symbols\n");
+                        return -1;
+                }
+                int digit = num[i] - '0';
+                convertedNum *= 10;
+                convertedNum += digit;
+
+        }
+        if (convertedNum > int_max) {
+                printf("Int overflow\n");
+                return -1;
+        }
+
+        if (sign) convertedNum *= -1;
+
+        return (int)convertedNum;
+
+
+}
+int main() {
+
+
+        char number[] = "123";
+        printf("%d", convert_str_to_num(number));
+
+        return 0;
 }
