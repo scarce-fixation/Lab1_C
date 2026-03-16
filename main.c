d59ea8722b2f6361f92a61bd74f8656ed7bc4a40
Bakhmutov_Ivan <vanya.bahmutov@yandex.ru>
2026-03-16 16:06:57 +0700

    Created a function that solves task 3.

--- main.c
+++ main.c
@@ -1,5 +1,7 @@
-#include <stdio.h>
-
+#include<stdio.h>
+#include<string.h>
+#define max_len 1000
+#define max_words 100
 int my_strcmp(char* a, char* b) {
         while (*a && *b && *a == *b) {
                 a++;
@@ -7,9 +9,52 @@ int my_strcmp(char* a, char* b) {
         }
         return (char)a - (char)b;
 }
-int main()
-{
+void str_sort_n_print(char* str) {
+        char* words[max_words];
 
-    return 0;
+        int word_count = 0;
+        int len = strlen(str);
+
+        if (len == 0) {
+                printf("Error: Parameter is empty\n");
+                return;
+        }
+        char* p = str;
+        words[0] = p;
+        word_count = 1;
+
+        while (*p) {
+                if (*p == ',' || *p == '.') {
+                        *p = '\0';
+                        if (*(p + 1) == ' ' || *(p + 1) == '\t' || *(p + 1) == '\n') {
+                                while (*p && !(*(p + 1) >= 'a' && *(p + 1) <= 'z')) p++;
+
+                                if (!*p) break;
+                        }
+                        if (*(p + 1) >= 'a' && *(p + 1) <= 'z') {
+                                words[word_count++] = p + 1;
+                        }
+                }
+                p++;
+        }
+
+        for (int i = 0; i < word_count - 1; i++) {
+                for (int j = i + 1; j < word_count; j++) {
+                        if (my_strcmp(words[i], words[j]) > 0) {
+                                char* temp = words[i];
+                                words[i] = words[j];
+                                words[j] = temp;
+                        }
+                }
+        }
+
+        for (int i = 0; i < word_count; i++) {
+                printf("%s\n", words[i]);
+        }
 }
+int main() {
 
+
+
+        return 0;
+}
